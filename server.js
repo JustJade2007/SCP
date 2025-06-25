dc33d21a4ba0032551e5a2c463fce4dcef9f2c4f
Jacob <jacobhite2007@gmail.com>
2025-06-25 13:00:02 -0400

    Render Setup + Password Encription

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+require('dotenv').config(); // This must be at the very top
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = 3000;
 
+// The secret is now loaded securely from the environment
+const DATABASE_URL = process.env.DATABASE_URL;
+
+// Connect to the database
+mongoose.connect(DATABASE_URL)
+  .then(() => console.log('Successfully connected to database'))
+  .catch(err => console.error('Database connection error:', err));
+
 app.get('/', (req, res) => {
-  res.send('SCP Foundation Server is Running');
+  res.send('SCP Foundation Server is Running.');
 });
 
 app.listen(PORT, () => {
